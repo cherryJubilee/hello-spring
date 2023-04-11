2775f9a5af5b1ec02638d72090863587105b540f
cherryJubilee <hyewon5026@gmail.com>
2023-04-12 01:04:58 +0900

    spring JdbcTemplate

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;   // 시스템에서 정해준 id
     private String name;
 
